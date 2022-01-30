3edff7a85f890513e96f0c7eb8dbd52924c627f8
Dinitha Nidushan <dinitha1296@gmail.com>
2022-01-30 19:27:28 +0530

    feat: add Dashboard and Product component
    -- Dashboard and Product components generated and added to the route.
    -- Menu items changed to reflect available routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductComponent } from './product/product.component';
+
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'products', component: ProductComponent},
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
