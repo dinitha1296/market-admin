bc9a0ee4d47b5c2e0503f942c2249f52f1f66b9c
Dinitha Nidushan <dinitha1296@gmail.com>
2022-02-01 09:12:14 +0530

    Revert "Revert "feat: add Dashboard and Product component""
    This reverts commit f49eadd03a6b966e1f471836f0fec8617b59103f.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TopNavigationComponent } from './top-navigation/top-navigation.component';
 import { SideNavigationComponent } from './side-navigation/side-navigation.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavigationComponent,
-    SideNavigationComponent
+    SideNavigationComponent,
+    DashboardComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
