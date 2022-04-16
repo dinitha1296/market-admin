6ba03ca765217e31db64e5d79974fb24b442db7d
Dinitha Nidushan <dinitha1296@gmail.com>
2022-04-16 08:21:38 +0530

    refactor: add search query to URL parameter
    -- Source of truth for current product search query was a class attribute.
    -- It is now moved to a URL paramter

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { mergeMap } from 'rxjs/operators';
 import { Product } from '../core/models/product.model';
 import { ProductService } from '../core/services/product.service';
 
@@ -14,23 +16,21 @@ export class ProductComponent implements OnInit {
   products?: Product[] = undefined;
 
   constructor(
-    private productService: ProductService
+    private productService: ProductService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    this.productService.getProductsByQuery(this.searchQuery)
-      .subscribe(prods => {
-        this.products = prods;
-        console.log(prods)
-      });
+    this.activatedRoute.queryParams.pipe(
+      mergeMap(params => {
+        this.searchQuery = params.search || "";
+        return this.productService.getProductsByQuery(this.searchQuery)
+      })
+    ).subscribe(products => this.products = products);
   }
 
   onSearchBtnClick(): void {
-    this.productService.getProductsByQuery(this.searchQuery)
-      .subscribe(prods => {
-        this.products = prods;
-        console.log(prods)
-      });
-    // this.productService.getProducts().subscribe(p => console.log(p));
+    this.router.navigate(['/products'], { queryParams: { search: this.searchQuery} });
   }
 }
